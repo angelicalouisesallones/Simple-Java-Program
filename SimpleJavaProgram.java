fa6f2f7f12baf3d5d01e96335da4f7ffe0e023a8
angelicalouisesallones <124368397+angelicalouisesallones@users.noreply.github.com>
2023-03-23 00:20:56 +0800

    Add files via upload

---
+++ SimpleJavaProgram.java
@@ -0,0 +1,8 @@
+public class SimpleJavaProgram
+{
+   public static void main(String[] args) {
+		System.out.println("My first Java program.");
+		System.out.println("The sum of 2 and 3 = " + 5);
+		System.out.println("7 + 8 = " + (7 + 8));
+	}
+ }
